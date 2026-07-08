055c9eb5d3e31d572c6180c7f1b2dd35e3a1f8ec
Meghna <meghna.ug23@nsut.ac.in>
2026-07-08 16:02:49 +0530

    problem detail page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser} f
 import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
+import ProblemPage from './pages/ProblemPage'
 import { Toaster } from 'react-hot-toast'
 import DashboardPage from './pages/DashboardPage'
 
@@ -16,6 +17,8 @@ function App() {
     <Route path = "/" element={!isSignedIn ? <HomePage/> : <Navigate to = {"/dashboard"}/> }/>
     <Route path = "/dashboard" element={isSignedIn ? <DashboardPage/> : <Navigate to = {"/"}/> }/>
     <Route path = "/problems" element={isSignedIn ?<ProblemsPage/>:<Navigate to = {"/"}/>}/>
+    <Route path = "/problem/:id" element={isSignedIn ?<ProblemPage/>:<Navigate to = {"/"}/>}/>
+
     </Routes>
     <Toaster position='top-left' toastOptions={{duration:3000}}/>
     </>
